749a744c641006d855c138dc5994361749ad308e
Bohdan <bogdandowgy2002@gmail.com>
2023-05-03 02:56:39 +0300

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://YOOBAMAWHEREISMYCLOTHES.github.io/my_portfolio/",
+  base: "/my_portfolio/",
   plugins: [react()],
 })
